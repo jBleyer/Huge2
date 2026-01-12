a34a9f3b6767be3345dbe44baaca012f0552ec57
jBleyer <162551911+jBleyer@users.noreply.github.com>
2026-01-12 10:48:04 +0100

    Update Admincontroller
    something ?

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -19,15 +19,20 @@ class AdminController extends Controller
      */
     public function index()
     {
-        $this->View->render('admin/index', array(
-                'users' => UserModel::getPublicProfilesOfAllUsers())
+        $this->View->render(
+            'admin/index',
+            array(
+                'users' => UserModel::getPublicProfilesOfAllUsers()
+            )
         );
     }
 
     public function actionAccountSettings()
     {
         AdminModel::setAccountSuspensionAndDeletionStatus(
-            Request::post('suspension'), Request::post('softDelete'), Request::post('user_id')
+            Request::post('suspension'),
+            Request::post('softDelete'),
+            Request::post('user_id')
         );
 
         Redirect::to("admin");
